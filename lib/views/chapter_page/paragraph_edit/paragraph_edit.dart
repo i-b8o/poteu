ed3902874385a94d8fe18a3c6ca6f5bc10ed482e
i-rm <ivanbogachenco@gmail.com>
2021-12-23 11:04:08 +0300

    adhfkldfsklh

--- lib/views/chapter_page/paragraph_edit/paragraph_edit.dart
+++ lib/views/chapter_page/paragraph_edit/paragraph_edit.dart
@@ -1,28 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:poteu/views/filter_all_paragraphs_page/filter_all_paragraphs_page.dart';
+import 'package:poteu/views/main_page/main_page.dart';
 
 class ParagraphEdit extends StatelessWidget {
   ParagraphEdit({Key? key, required this.text}) : super(key: key);
   final String text;
   TextEditingController _controller = new TextEditingController();
-
+  // void _change()
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: new IconButton(
+          icon: new Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(
+              context,
+            );
+          },
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const FilterAllParagraphsPage()),
+              );
+            },
+            icon: const Icon(Icons.search),
+          ),
+        ],
+        title: const Text("ПОТЭУ-903н-2020"),
+        centerTitle: true,
+      ),
       body: ListView(
         children: <Widget>[
           TextFormField(
               maxLines: null,
               initialValue: text,
-              style: TextStyle(fontSize: 22),
               onChanged: (value) => print(value)),
           new RaisedButton(
-            onPressed: () {
-              // You can also use the controller to manipuate what is shown in the
-              // text field. For example, the clear() method removes all the text
-              // from the text field.
-              _controller.clear();
-            },
-            child: new Text('CLEAR'),
+            onPressed: () {},
+            child: new Text('Сохранить'),
           ),
         ],
       ),
