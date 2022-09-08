03c1af8a3e7383dc42e518b0207298c37499933e
bogach-ivan <blackview1617@gmail.com>
2022-09-08 18:52:30 +0300

    sentry url

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 
 import 'package:local_storage_regulation_api/local_storage_regulation_api.dart';
+import 'package:poteu/sentryurl.dart';
 
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -15,7 +16,6 @@ import 'package:tts/tts.dart';
 import 'presentation/screens/app/app.dart';
 import 'repository/regulation_repository.dart';
 
-// TODO you can listen it in car.
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(
@@ -31,8 +31,7 @@ Future<void> main() async {
       regulationApi: regulationApi, spApi: sp, sqliteApi: sqlite, ttsApi: tts);
 
   await SentryFlutter.init(
-    (options) => options.dsn =
-        'https://4cff0764c8fd4601bef3333115846bdf@o1380632.ingest.sentry.io/6693937',
+    (options) => options.dsn = sentryUrl,
     appRunner: () => runApp(
       App(regulationRepository: regulationRepository),
     ),
